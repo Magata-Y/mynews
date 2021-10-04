64ef4bdc9e6a601169f5b651c9f42a9b512607a7
名前 <>
2021-10-04 05:06:54 +0000

    test

--- task02.php
+++ task02.php
@@ -3,20 +3,24 @@
 $a = 3;
 $b = 7;
 echo $a + $b;
+echo "\n";
 
 /* 課題2 */
 $array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
 echo $array_month[7];
+echo "\n";
 
 /* 課題3 */
 $hello ="Hello, ";
 $name = "Yoshihiro Magata";
 $world = "'s World!";
 echo $hello . $name . $world;
+echo "\n";
 
 /* 課題4 */
 $tech_boost = "tech";
 echo $tech_boost . "boost";
+echo "\n";
 
 /* 課題5 */
 $calender_2018 = [
