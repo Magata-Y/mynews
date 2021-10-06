0dbe8f2d910fa873ea8fc7a0e4fc7778b85ae65a
名前 <>
2021-10-06 05:51:43 +0000

    task11-4

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>MyProfile</title>
+        <title>MyNews</title>
     </head>
     <body>
-        <h1>Myプロフィール作成画面</h1>
+        @extends('layouts.admin')
+        
+        @section('title', 'My プロフィールの作成')
+        
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>My プロフィール</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
     </body>
 </html>
