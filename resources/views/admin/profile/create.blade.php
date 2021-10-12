1fa01e0512b6845e19642e65f11cd544eaa489e7
名前 <>
2021-10-12 05:58:37 +0000

    Laravel13 課題4

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -51,6 +51,7 @@
                                 </div>
                             </div>
                             {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
                         </form>
                     </div>
                 </div>
